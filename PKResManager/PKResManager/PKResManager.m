ae9b90a7d74a2dbcd7c91da9c2f1f0204c316210
Zhong Sheng <passerbycrk@gmail.com>
2012-07-17 23:13:10 +0800

    delete debug code

--- PKResManager/PKResManager/PKResManager.m
+++ PKResManager/PKResManager/PKResManager.m
@@ -119,9 +119,6 @@ customStyleArray = _customStyleArray;
 //    NSLog(@"resOtherCache:%@",self.resOtherCache);
     
     NSLog(@"all res object count:%d",self.resObjectsArray.count);
-    if (self.resObjectsArray.count<=1) {
-        NSLog(@" nani :%@",self.resObjectsArray);
-    }
     // change style
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
     dispatch_async(queue, ^{
