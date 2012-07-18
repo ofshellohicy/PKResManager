b9b1ce9b1fc879dd4e153fafd7ae8ec2a381305d
Zhong Sheng <passerbycrk@gmail.com>
2012-07-18 10:52:47 +0800

    update demo

---
+++ PKResManager/demo/PKStyleListViewController.h
@@ -0,0 +1,13 @@
+//
+//  PKStyleListViewController.h
+//  PKResManager
+//
+//  Created by zhong sheng on 12-7-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKStyleListViewController : UIViewController
+
+@end
---
+++ PKResManager/demo/PKStyleListViewController.m
@@ -0,0 +1,43 @@
+//
+//  PKStyleListViewController.m
+//  PKResManager
+//
+//  Created by zhong sheng on 12-7-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PKStyleListViewController.h"
+
+@interface PKStyleListViewController ()
+
+@end
+
+@implementation PKStyleListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
